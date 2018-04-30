b9489a75fc8345878b5bc2e0d74cb69edba6af5b
Tolulope24 <36515830+Tolulope24@users.noreply.github.com>
2018-04-30 14:19:12 -0400

    Initial commit

---
+++ DynamicPictureBoxForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DynamicPicBoxToluA
+{
+    public partial class frmDynamicPictureBox : Form
+    {
+        public frmDynamicPictureBox()
+        {
+            InitializeComponent();
+        }
+        private void GeneratePictureBox()
+        {
+            // Generate each of the picture boxes on the form
+            GeneratePictureBox(61, 78);
+            GeneratePictureBox(464, 78);
+
+        }
+
+
+        private void frmDynamicPictureBox_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
